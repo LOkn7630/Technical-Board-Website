6daf487b64834b8cb6c9738bbcca0b3229f81bc9
LOkn7630 <loknath@iitg.ac.in>
2021-07-01 22:28:28 +0530

    mains

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,30 +1,3 @@
-// let el = document.querySelector(".tabs");
-const listOfTabs = document.querySelectorAll(".tablink");
-
-for (let i = 0; i < listOfTabs.length; i++) {
-  listOfTabs[i].addEventListener("click", (e) => {
-    const clickedTab = e.target.parentNode;
-    clickedTab.classList.add("active");
-
-    const tabs = e.target.parentNode.parentNode.children;
-    for (let tab of tabs) {
-      if (tab !== e.target.parentNode && tab.classList[0] !== "indicator")
-        tab.classList.remove("active");
-    }
-  });
-}
-
-// $(".tablink").click(function() {
-      
-//     // Select all list items
-//     var listItems = $(".tablink");
-      
-//     for (let i = 0; i < listItems.length; i++) {
-//         listItems[i].classList.remove("active");
-//     }
-//           this.classList.add("active");
-// });
-
 function openPage(pageName) {
   var i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
